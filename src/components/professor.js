1cb1774564b476c551187930f267f76c7512f598
Araliatrix <agentisaacson@gmail.com>
2018-08-17 14:35:28 -0600

    updated routing

--- src/components/professor.js
+++ src/components/professor.js
@@ -1,15 +1,57 @@
+import React, { Component } from "react";
 
-import React, { Component } from 'react';
-import ProfessorImg from './professor-img';
+import { NavLink } from "react-router-dom";
 
 class Professor extends Component {
-    render() {
-        return (
-            <div>
-                <ProfessorImg />
-            </div>
-        );
-    };
-};
+	render() {
+		return (
+			<div id="wrapper">
+				<NavLink to="/">Rate My Professor</NavLink>
+
+				<div className="header-img">
+					<div className="columns">
+						<h2>PROF MAD LIB</h2>
+						<div className="pin">
+							<img
+								className="front"
+								src="https://www.ragnbonemanmusic.com/wp-content/themes/ragnboneman/assets/img/ragsold.jpg"
+								alt="professor"
+							/>
+						</div>
+					</div>
+				</div>
+				<div className="attributes">
+					<li>
+						<b>Department:</b> COMPUTER SCIENCE
+					</li>
+					<li>
+						<b>School:</b> TECHNOLOGY
+					</li>
+					<li>
+						<b>Height:</b> 6ft5
+					</li>
+					<li>
+						<b>Favorite Book:</b> The Gods Must Be Crazy
+					</li>
+					<li>
+						<b>Office:</b> CB 311 - E
+					</li>
+					<li>
+						<b>Email:</b> madlib@bottega.edu
+					</li>
+					<li>
+						<b>Mail Code:</b> 133
+					</li>
+					<li>
+						<b>Phone:</b> (801)-777-7777
+					</li>
+					<li>
+						<b>Height:</b> 6ft5
+					</li>
+				</div>
+			</div>
+		);
+	}
+}
 
 export default Professor;
--- src/components/results.js
+++ src/components/results.js
@@ -14,14 +14,16 @@ export default class Results extends Component {
 		return (
 			<div className="results-wrapper">
 				<NavLink to="/">Rate My Professor</NavLink>
-				<div className="icon">
-					{" "}
-					<img src="\assets\2000px-University_hat_icon1.png" />
-				</div>
-				<div className="professor-name">Professor Xavier</div>
-				<div className="school-name">Gifted Youngsters</div>
-				<div className="subject-name">Anger Managment</div>
-				<div className="rating">*****</div>
+				<NavLink to="/professor">
+					<div className="icon">
+						{" "}
+						<img src="\assets\2000px-University_hat_icon1.png" />
+					</div>
+					<div className="professor-name">Professor Xavier</div>
+					<div className="school-name">Gifted Youngsters</div>
+					<div className="subject-name">Anger Managment</div>
+					<div className="rating">*****</div>
+				</NavLink>
 			</div>
 		);
 	}
