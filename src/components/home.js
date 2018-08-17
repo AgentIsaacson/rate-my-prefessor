c53b0a3839d4f7f540db76bb600d84229a58cbd1
Joe Anderson <joseph.anderson423@gmail.com>
2018-08-17 13:32:07 -0600

    1st homepage push

--- src/components/home.js
+++ src/components/home.js
@@ -4,8 +4,29 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <h1>DevCamp React Starter</h1>
+        <div className="navbar">
+          <div>
+            <a className="active" href="#">Rate My Professor</a>
+          </div>
+          <div>
+            <input type="text" placeholder="SEARCH FOR A PROFESSOR OR SCHOOL"></input>
+          </div>
+        </div>
+
+        <div className="second-search">
+          <button>Find a professor</button>
+          <button>Find a school</button>
+          <button>Rate a professor</button>
+        </div>
+
+        <div className="body">
+          <p className="search">Search.</p>
+          <p className="rate">Rate.</p>
+          <p className="know">Know.</p>
+        </div>
       </div>
+
+      
     );
   }
 }
