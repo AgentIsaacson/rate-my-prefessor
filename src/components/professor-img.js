8a33a719857dbb780b7de772d831f5b86faa0a09
manuelade <adesotu2016@gmail.com>
2018-08-17 13:27:10 -0600

    styled components

--- src/components/professor-img.js
+++ src/components/professor-img.js
@@ -4,12 +4,26 @@ class ProfessorImg extends Component {
     render() {
         return (
             <div id="wrapper">
-                <h2>Professor Blake Shelton</h2>
-                <div id="columns">
-                    <div className="pin">
-                        <img className='front' src="https://www.ragnbonemanmusic.com/wp-content/themes/ragnboneman/assets/img/ragsold.jpg" alt=""
-                        />
+                <div className="header-img">
+                    <div className="columns">
+                        <h2>PROF MAD LIB</h2>
+                        <div className="pin">
+                            <img className='front' src="https://www.ragnbonemanmusic.com/wp-content/themes/ragnboneman/assets/img/ragsold.jpg" alt="professor"
+                            />
+                        </div>
                     </div>
+                </div>
+                <div className="attributes">
+                    <li><b>Department:</b> COMPUTER SCIENCE</li>
+                    <li><b>School:</b> TECHNOLOGY</li>
+                    <li><b>Height:</b> 6ft5</li>
+                    <li><b>Favorite Book:</b> The Gods Must Be Crazy</li>
+                    <li><b>Office:</b> CB 311 - E</li>
+                    <li><b>Email:</b> madlib@bottega.edu</li>
+                    <li><b>Mail Code:</b> 133</li>
+                    <li><b>Phone:</b> (801)-777-7777</li>
+                    <li><b>Height:</b> 6ft5</li>
+
                 </div>
             </div>
         );
@@ -17,3 +31,4 @@ class ProfessorImg extends Component {
 }
 
 export default ProfessorImg;
+
